0f910a792f9bbde9900088642bfa39f8021af729
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2024-11-13 15:58:40 -0800

    Use State with react

---
+++ src/components/Images.js
@@ -0,0 +1,12 @@
+
+const myImages =[
+    {
+        title: 'Tom Cruz',
+        img: ' https://th.bing.com/th/id/OIP.rZ8FY9_8muRObhxs6TMZegHaLH?w=197&h=296&c=7&r=0&o=5&dpr=1.6&pid=1.7',
+    },
+    {
+        title: 'tom and jerry',
+        img: 'https://th.bing.com/th/id/OIP.ZOEytN6LxvuZ5m43SDAzlAHaLI?w=197&h=297&c=7&r=0&o=5&dpr=1.6&pid=1.7 '
+    }
+]
+export default myImages;
